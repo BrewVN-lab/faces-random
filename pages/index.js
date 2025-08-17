d89e931d56b7553193f139c379288610aac3f14d
BrewVN <brewvn.ltd@gmail.com>
2025-08-17 22:42:37 +0700

    Update index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Ảnh từ Proxy Vercel</h1>
-      <img src="/api/proxy" alt="Fake Person" style={{ borderRadius: "10px" }} />
-      <p>Reload trang để đổi ảnh mới.</p>
+      <img src="/api/proxy" style={{ borderRadius: "10px" }} />
     </div>
   );
 }
