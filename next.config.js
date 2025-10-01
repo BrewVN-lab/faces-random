8095513b8c638d34a31d039265b077fb0a24e13d
BrewVN <brewvn.ltd@gmail.com>
2025-10-01 23:28:21 +0700

    Update

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Bật chế độ build standalone (tạo .next/standalone)
+  output: 'standalone',
+}
+
+module.exports = nextConfig
